a40e1de7d5e77235b772f859e87e72bd85ccf9ce
Valnar Dewan <mail@story75.com>
2014-08-31 19:22:53 +0200

    LISENCE

--- src/DependencyInjection/Container.php
+++ src/DependencyInjection/Container.php
@@ -1,13 +1,24 @@
 <?php
 /**
- * This software is provided 'as-is', without any express or implied warranty.
- * In no event will the authors be held liable for any damages arising from the use of this software.
+ * Copyright (C) 2014  Alexander Schmidt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * @author     Alexander Schmidt <mail@story75.com>
  * @copyright  Copyright (c) 2014, Alexander Schmidt
  * @version    1.0
  * @date       2014-07-20
- * @package    Bonefish
- * @subpackage DependencyInjection
+ * @package    Bonefish\DependencyInjection
  */
 
 namespace Bonefish\DependencyInjection;
